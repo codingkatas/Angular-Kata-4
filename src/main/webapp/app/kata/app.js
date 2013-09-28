179bc02b83b2525fa6f108c5035380900ade4ad0
Soeren Pedersen <speconsult@gmail.com>
2013-09-28 20:56:52 +0200

    added spinner to ajax loading

--- src/main/webapp/app/kata/app.js
+++ src/main/webapp/app/kata/app.js
@@ -10,4 +10,50 @@ namespace('kata');
 
 var kataModule = angular.module('Kata', ['ngResource']);
 
+kataModule.factory('httpInterceptor', function ($q, $rootScope) {
+
+    return {
+
+        // optional method
+        'request':function (config) {
+            $rootScope.showOverlay = true;
+            return config || $q.when(config);
+        },
+
+        // optional method
+        'requestError':function (rejection) {
+            $rootScope.showOverlay = false;
+            return $q.reject(rejection);
+        },
+
+
+        // optional method
+        'response':function (response) {
+            $rootScope.showOverlay = false;
+            return response || $q.when(response);
+        },
+
+        // optional method
+        'responseError':function (rejection) {
+            $rootScope.showOverlay = false;
+            return $q.reject(rejection);
+        }
+    }
+});
+
+kataModule.config(['$httpProvider',
+    function ($httpProvider) {
+        $httpProvider.interceptors.push('httpInterceptor');
+    }
+]);
+
+
+kataModule.run(function($rootScope) {
+
+    $rootScope.showOverlay = false;
+});
+
+
+
+
 
